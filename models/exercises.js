ff88e97bde9b12b687131f313b06a5425be6707e
Andrew <andrewyen64@gmail.com>
2020-09-12 15:32:02 -0700

    Add virtual method to get total duration

--- models/exercises.js
+++ models/exercises.js
@@ -1,41 +1,54 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ExerciseSchema = new Schema({
-    day: {
-        type: Date,
-        default: () => new Date()
+const ExerciseSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [{
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter the name of this exercise"
+            },
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter the type of exercise"
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            duration: {
+                type: Number,
+                required: "Enter the duration for this exercise in minutes"
+            },
+            distance: {
+                type: Number
+            }
+        }]
     },
-    exercises: [{
-        name: {
-            type: String,
-            trim: true,
-            required: "Enter the name of the exercise"
-        },
-        type: {
-            type: String,
-            trim: true,
-            required: "Enter the type of exercise"
-        },
-        weight: {
-            type: Number
-        },
-        sets: {
-            type: Number
-        },
-        reps: {
-            type: Number
-        },
-        duration: {
-            type: Number,
-            required: "Enter the duration for this exercise in minutes"
-        },
-        distance: {
-            type: Number
+        {
+            toJSON: {
+            virtuals: true
+            }
         }
-    }]
+);
+
+ExerciseSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+    }, 0);
 });
 
-const Exercises = mongoose.model("Exercises", ExerciseSchema);
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
-module.exports = Exercises;
+module.exports = Exercise;
